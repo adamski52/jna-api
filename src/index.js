7603f2b7d559f97a7a8f50bb564e8e48a3b9a2aa
Jon Adamski <jadamski@drfirst.com>
2018-02-10 22:56:11 -0500

    moved github integration to api app.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 var express = require("express"),
     app = express(),
     cors = require("cors"),
+    proxy = require("http-proxy-middleware"),
     router = require("./router"),
     db = require("./db"),
-    port = 8080;
+    port = 8080,
+    githubProxy = proxy({
+        target: "https://api.github.com/",
+        pathRewrite: {
+            "^/git": ""
+        },
+        changeOrigin: true,
+        onProxyReq: function(proxyReq, req, res) {
+            proxyReq.setHeader("Authentication", "token " + process.env.GITHUB_TOKEN);
+            proxyReq.setHeader("User-Agent", "maintaincomposure.com");
+            proxyReq.setHeader("Content-Type", "application/json");
+        }
+    });
 
 db.connect();
 
 app.use(cors());
 app.use("/api", router);
+app.use("/git", githubProxy);
+
 
 app.listen(port);
 
